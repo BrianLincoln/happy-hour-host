3c138410f4f31238f98be693b4c1a054a7202ae9
Brian Lincoln <brianlincoln612@gmail.com>
2017-09-15 10:55:30 -0500

    ssl testing

--- server/index.js
+++ server/index.js
@@ -4,21 +4,11 @@
 
 const app = require('./app');
 const https = require('https');
-const fs = require('fs');
-var secretConfig = require('../secret-config');
-
-const options = {
-  cert: fs.readFileSync('./sslcert/fullchain.pem'),
-  key: fs.readFileSync('./sslcert/privkey.pem'),
-  passphrase: secretConfig.certPassphrase,
-  requestCert: false,
-  rejectUnauthorized: false
-};
+var secretConfig = require('./secret-config');
 
 const PORT = 80;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
-
-https.createServer(options, app).listen(443);
+https.createServer(secretConfig.certOptions, app).listen(443);
