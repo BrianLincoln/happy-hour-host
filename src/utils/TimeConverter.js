e81e1287d4df807bd5ceaa3b8ce5adf6b78faa85
Brian Lincoln <brianlincoln612@gmail.com>
2018-08-05 18:42:53 -0500

    fixed bugs with formatting times

--- src/utils/TimeConverter.js
+++ src/utils/TimeConverter.js
@@ -1,6 +1,16 @@
+function formatHours(hours) {
+  let result = parseInt(hours, 10);
+
+  if (result === 0) {
+    result = 12;
+  }
+
+  return result > 12 ? result - 12 : result;
+}
+
 export default function timeConverter(time) {
   const hours = time.substring(0, 2);
-  const formattedHours = hours > 12 ? hours - 12 : hours;
+  const formattedHours = formatHours(hours);
   const minutes = time.substring(3, 5);
   const suffix = hours < 12 ? 'am' : 'pm';
 
